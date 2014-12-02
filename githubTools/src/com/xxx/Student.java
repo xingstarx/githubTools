0cfe3cea64a73d8e0a865a7efd4294bacfb4d2cb
xing_star <823952375@qq.com>
2014-12-03 00:09:40 +0800

    init this project

---
+++ githubTools/src/com/xxx/Student.java
@@ -0,0 +1,30 @@
+package com.xxx;
+
+public class Student {
+	private String name;
+	private int id;
+	private String address;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", id=" + id + ", address=" + address
+				+ "]";
+	}
+}
---
+++ githubTools/src/com/xxx/StudentGUI.java
@@ -0,0 +1,41 @@
+package com.xxx;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class StudentGUI {
+	public List<Student> getList(){
+		List<Student> list=new ArrayList<Student>();
+		for(int i=0;i<5;i++){
+			Student s=new Student();
+			s.setAddress("baidu"+(i+1));
+			s.setId((i+1));
+			s.setName("xxx"+(i+1));
+			list.add(s);
+		}
+		return list;
+	}
+	public void show(){
+		JFrame frame=new JFrame("User by xxx");
+		JPanel panel=new JPanel();
+		JTextArea textArea=new JTextArea();
+		panel.add(textArea);
+		List<Student> list=getList();
+		for(int i=0;i<list.size();i++){
+			textArea.append(list.get(i).toString()+"\r\n");
+		}
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		frame.setBounds(400, 400, 400, 400);
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new StudentGUI().show();
+	}
+}
